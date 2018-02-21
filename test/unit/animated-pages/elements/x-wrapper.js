b6dac7ee02dddb6977c2805732a603465d669834
Josef Reinhard Brandl <mail@josefbrandl.de>
2018-02-21 21:05:09 +0100

    Fix page switching

--- test/unit/animated-pages/elements/x-wrapper.js
+++ test/unit/animated-pages/elements/x-wrapper.js
@@ -4,6 +4,7 @@ import '../../../../lib/animated-pages.js'
 const template = html`
 <animated-pages id="animated-pages">
   <template name="1"><div>1</div></template>
+  <template name="2"><div>2</div></template>
 </animated-pages>`
 
 export default class XWrapper extends Element {
--- test/unit/animated-pages/suite.js
+++ test/unit/animated-pages/suite.js
@@ -19,18 +19,57 @@ describe('<animated-pages>', () => {
     }
   })
 
-  it('can select a page via a string literal', async () => {
-    let createAnimationInput
-    animatedPages.createAnimation = (x) => { createAnimationInput = x }
+  it('can switch between pages sequentially', async () => {
+    function study (selector) {
+      const el = getElement(selector)
+      if (el) { return { text: el.textContent, hidden: el.hidden } }
+    }
+
+    let createAnimationInput1
+    animatedPages.createAnimation = (x) => { createAnimationInput1 = x }
     animatedPages.selected = '1'
     await animatedPages.finished
-    expect(getTextContent('div:nth-of-type(1)')).to.equal('1')
-    expect(createAnimationInput).to.eql({
+    expect(study('div:nth-of-type(1)')).to.eql({ text: '1', hidden: false })
+    expect(createAnimationInput1).to.eql({
       elementA: undefined,
       elementB: getElement('div:nth-of-type(1)'),
       templateNameA: undefined,
       templateNameB: '1',
       idA: undefined,
+      idB: '1',
+      dataA: undefined,
+      dataB: undefined
+    })
+
+    let createAnimationInput2
+    animatedPages.createAnimation = (x) => { createAnimationInput2 = x }
+    animatedPages.selected = '2'
+    await animatedPages.finished
+    expect(study('div:nth-of-type(1)')).to.eql({ text: '1', hidden: true })
+    expect(study('div:nth-of-type(2)')).to.eql({ text: '2', hidden: false })
+    expect(createAnimationInput2).to.eql({
+      elementA: getElement('div:nth-of-type(1)'),
+      elementB: getElement('div:nth-of-type(2)'),
+      templateNameA: '1',
+      templateNameB: '2',
+      idA: '1',
+      idB: '2',
+      dataA: undefined,
+      dataB: undefined
+    })
+
+    let createAnimationInput3
+    animatedPages.createAnimation = (x) => { createAnimationInput3 = x }
+    animatedPages.selected = '1'
+    await animatedPages.finished
+    expect(study('div:nth-of-type(1)')).to.eql({ text: '1', hidden: false })
+    expect(study('div:nth-of-type(2)')).to.eql({ text: '2', hidden: true })
+    expect(createAnimationInput3).to.eql({
+      elementA: getElement('div:nth-of-type(2)'),
+      elementB: getElement('div:nth-of-type(1)'),
+      templateNameA: '2',
+      templateNameB: '1',
+      idA: '2',
       idB: '1',
       dataA: undefined,
       dataB: undefined
